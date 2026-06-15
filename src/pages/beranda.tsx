7b7f669b1b5d9214e5d71a1af0437b123ddf0f7b
baraboy <rizky.dwiixg36@gmail.com>
2026-06-15 17:17:01 +0800

    ganti warna dasar hero section

--- src/pages/beranda.tsx
+++ src/pages/beranda.tsx
@@ -54,12 +54,12 @@ export default function Beranda() {
         <div
           className="absolute inset-0 z-0"
           style={{
-            backgroundImage: "url('/balai.webp')",
+            backgroundImage: "url('/bbws nt.jpg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <div className="absolute inset-0 bg-[#002d62]/90" />
+          <div className="absolute inset-0 bg-[#0d348a]/90" />
         </div>
         <div className="relative z-10 grid grid-cols-1 md:grid-cols-[2fr,1fr] items-center gap-8 md:gap-16 max-w-7xl mx-auto">
           {/* Left: Text Content */}
