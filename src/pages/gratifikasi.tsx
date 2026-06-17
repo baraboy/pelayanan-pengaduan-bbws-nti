4f65eda0b8b7a0fd0468c86eb739a7943b80b147
baraboy <rizky.dwiixg36@gmail.com>
2026-06-17 12:22:08 +0800

    update icon reload

--- src/pages/gratifikasi.tsx
+++ src/pages/gratifikasi.tsx
@@ -243,7 +243,7 @@ export default function Gratifikasi() {
                     className="p-1 hover:opacity-80 transition-opacity"
                     aria-label="Reload Captcha"
                   >
-                    <img src="/reload.png" alt="Reload" className="h-2 w-2" />
+                    <img src="/reload.png" alt="Reload" className="h-6 w-6" />
                   </button>
                   <LoadCanvasTemplateNoReload />
                 </div>
